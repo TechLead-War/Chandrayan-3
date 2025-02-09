c0885143e26d0a47dcec78d3ee021482a95ecf18
Ayush-Techie <ayush.kapri@julofinance.com>
2025-02-09 22:31:44 +0530

    v1 version, this have some bugs in final descent algorithm.

---
+++ terrain.py
@@ -0,0 +1,12 @@
+class Terrain:
+    def __init__(self):
+        self.craters = []
+        for _ in range(6):
+            cx = random.randint(LEFT_BOUND+50, RIGHT_BOUND-50)
+            r = random.randint(30,80)
+            self.craters.append((cx, r))
+    def draw(self, screen):
+        pygame.draw.rect(screen, DARK, (0, HEIGHT-50, WIDTH, 50))
+        ground_y = HEIGHT-50
+        for (cx, r) in self.craters:
+            pygame.draw.circle(screen, BROWN, (cx, ground_y), r)
